78d68688f66f2306fff483cb78d5b88703ded43e
Angela <angelalamluu@gmail.com>
2020-12-05 00:58:37 -0800

    commit tree e90ae6185809618a8f7873998bbe983747f0c285

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount, :bankaccount
+  
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if @sender.balance < amount || !valid?
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    elsif valid? && status != "complete"
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      @sender.balance += amount
+      @receiver.balance -= amount
+      @status = "reversed"
+    end
+
+  end
 end
